39c28e8c88ac106c9582d4a0b02d3f312b05eb61
Nick C <69825418+nicholascuneo@users.noreply.github.com>
2025-01-13 00:31:00 -0800

    Create settings file and initialize
    Settings file contains settings class to store all
    settings for game. Initialize with window size and
    background color.

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """Class to store all settings for Cosmic Invaders"""
+
+    def __init__(self):
+        """Initialize game settings"""
+
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0, 20, 50)
