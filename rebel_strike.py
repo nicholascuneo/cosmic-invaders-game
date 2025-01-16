9910dda130fdbde0068f7850e3eecf2c8c4c4768
Nick C <69825418+nicholascuneo@users.noreply.github.com>
2025-01-15 21:21:02 -0800

    Refactor keydown and keyup events code

--- rebel_strike.py
+++ rebel_strike.py
@@ -36,19 +36,26 @@ class RebelStrike:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    # Move ship to the right
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    # Move ship to the left
-                    self.ship.moving_left = True
-
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    # Stop moving the ship
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to key press down events"""
+        if event.key == pygame.K_RIGHT:
+            # Move ship to the right
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            # Move ship to the left
+            self.ship.moving_left = True
+
+    def _check_keyup_events(self, event):
+        """Respond to key release up events"""
+        if event.key == pygame.K_RIGHT:
+            # Stop moving the ship
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
