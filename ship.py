2a342c35fa9dc88315ff4c48bde30250b47ce5eb
Nick C <69825418+nicholascuneo@users.noreply.github.com>
2025-01-15 21:08:36 -0800

    Add params to modify speed and limit range of ship

--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
     def __init__(self, rs_game):
         """Initialize ship and set its starting position"""
         self.screen = rs_game.screen
+        self.settings = rs_game.settings
         self.screen_rect = rs_game.screen.get_rect()
 
         # Load ship image and get its rect.
@@ -16,16 +17,24 @@ class Ship:
         # Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
+
         # Movement flags
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
-        """Update ship's position based on movement flag"""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        """Update ship's position based on movement flags"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+            # self.rect.x += 1
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+            # self.rect.x -= 1
+
+        # Update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw ship at its current location"""
