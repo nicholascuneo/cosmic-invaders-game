6a1f46c0535e9a620b50936bf192551471385074
Nick C <69825418+nicholascuneo@users.noreply.github.com>
2025-01-20 00:18:51 -0800

    Update bullet speed to improve gameplay

--- settings.py
+++ settings.py
@@ -21,11 +21,11 @@ class Settings:
         self.ship_speed = 1.5
 
         # Bullet settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (255, 0, 0)
-        self.bullets_allowed = 5
+        self.bullets_allowed = 7
 
         # Enemy ship settings
         self.enemy_speed = 1.0
